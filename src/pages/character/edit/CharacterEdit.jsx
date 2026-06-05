7535e5a3abf77f45c810b15b7a36c69a6c147ea9
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-06-05 16:21:32 +0200

    feat(character): add character edit page, under construction

---
+++ src/pages/character/edit/CharacterEdit.jsx
@@ -0,0 +1,5 @@
+import UnderConstruction from '../../../components/underConstruction/UnderConstruction.jsx'
+
+export default function CharacterEdit() {
+    return <UnderConstruction pageTitle="Character Editor" />
+}
