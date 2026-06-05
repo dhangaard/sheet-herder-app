5845ffaca32a1ccf08e46c5b1051505811afadb3
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-06-05 10:40:20 +0200

    feat(characters): add get and delete to character service

--- src/services/characterService.js
+++ src/services/characterService.js
@@ -1,9 +1,19 @@
 import { BASE_URL, throwIfError } from './apiClient';
 import { buildOptions } from './authService'
 
-
 export async function getCharacters() {
     const response = await fetch(BASE_URL + `character-sheets`, buildOptions('GET', true));
     await throwIfError(response, 'Could not load character sheets');
     return response.json();
+}
+
+export async function getCharacter(id) {
+    const response = await fetch(BASE_URL + `character-sheets/${id}`, buildOptions('GET', true));
+    await throwIfError(response, `Could not load character sheet | id: ${id}`);
+    return response.json();
+}
+
+export async function deleteCharacter(id) {
+    const response = await fetch(BASE_URL + `character-sheets/${id}`, buildOptions('DELETE', true));
+    await throwIfError(response, `Could not delete character sheet | id: ${id}`);
 }
